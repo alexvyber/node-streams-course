afb1913c96dcc8ac0205ffc69bff6834cad8912a
Alexey Sokolov <alexvyber@gmail.com>
2023-08-30 21:02:10 +0300

    finished 12-13 videos

---
+++ working/pipe-vs-pipeline/index.mjs
@@ -0,0 +1,15 @@
+import { createServer } from "node:http"
+import { createReadStream } from "node:fs"
+import { pipeline } from "node:stream/promises"
+
+const file = createReadStream("./big.file")
+const file2 = createReadStream("./big.file")
+
+createServer((req, res) => {
+file.pipe(res)
+}).listen(3000, () => console.log("started"))
+
+createServer(async (req, res) => {
+  file.pipe(res)
+  await pipeline(file2, res)
+  }).listen(3001, () => console.log("started"))
